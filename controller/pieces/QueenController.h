42d355733f1e465126d3731fa0aa6a541f72b7bf
Louis Jacquelin <jacquelinlouis@gmail.com>
2019-07-07 15:51:13 +0200

    Fix QueenController import

--- controller/pieces/QueenController.h
+++ controller/pieces/QueenController.h
@@ -9,4 +9,4 @@ public:
     std::vector<Position> possibleMoves(BoardModel & board, const Position & origin) override;
 };
 
-#endif CONTROLLER_QUEEN_CONTROLLER_H_
+#endif // CONTROLLER_QUEEN_CONTROLLER_H_
