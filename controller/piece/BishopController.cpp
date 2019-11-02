73a9ab1413d30ca8cd71d9f3670e948cc380d3b8
Louis Jacquelin <jacquelinlouis@gmail.com>
2019-11-02 10:00:43 +0100

    Fix bishop controller possible moves

--- controller/piece/BishopController.cpp
+++ controller/piece/BishopController.cpp
@@ -1,14 +1,21 @@
 #include "BishopController.h"
 
+int pow(int a, int b) {
+    if (b < 1) {
+        return 1;
+    }
+    return a * pow(a, b - 1);
+}
+
 std::vector<Position> BishopController::possibleMoves(const Position & origin) {
     std::vector<Position> moves;
-    for (int j = 0; j < 4; ++j) {
-        int deltaX = (-1 + 2 * (j % 2));
-        int deltaY = (-1 + 2 * (j % 3));
-        for (int i = 0; i < BOARD_SIZE; ++i) {
-            Position move = Position(origin).addX(deltaX * i).addY(deltaY * i);
+    for (int i = 1; i < BOARD_SIZE; ++i) {
+        for (int j = 1; j < 5; ++j) {
+            int x = origin[X] + (i * pow(-1, j % 2));
+            int y = origin[Y] + (i * pow(-1, j % 3));
+            Position move(x, y);
             if (BoardModel::inBoard(move))
-                moves.push_back(move);
+                    moves.push_back(move);
         }
     }
     return moves;
